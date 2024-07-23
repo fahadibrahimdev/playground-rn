bcd78db07c877458bfa149bf83b54fba3009fa39
Fahad Ibrahim <fibrahim@ssidecisions.com>
2024-07-23 16:53:40 +0500

    Code cleanup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Animated, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import MainAnimatedHeader from "rn-dev-dock";
-// import AnimatedHeader from "./AnimatedHeader";
-// import { AnimatedHeader } from 'my-react-native-package-test-fahad';
-// import { AnimatedHeader } from 'my-react-native-package-test-fahad';
-
-
 
 function App(): React.JSX.Element {
   const [scrollY] = useState<Animated.Value>(new Animated.Value(0));
   const DATA = [
     { title: "Section 0", data: ["Item 1", "Item 2", "Item 3"] },
-    { title: "Section 1", data: ["Item 1", "Item 2", "Item 3"] },
-    { title: "Section 2", data: ["Item 4", "Item 5", "Item 6"] },
-    { title: "Section 1", data: ["Item 1", "Item 2", "Item 3"] },
-    { title: "Section 2", data: ["Item 4", "Item 5", "Item 6"] },
-    { title: "Section 1", data: ["Item 1", "Item 2", "Item 3"] },
     { title: "Section 1", data: ["Item 1", "Item 2", "Item 3"] },
     { title: "Section 2", data: ["Item 4", "Item 5", "Item 6"] },
     { title: "Section 1", data: ["Item 1", "Item 2", "Item 3"] },
@@ -33,36 +23,12 @@ function App(): React.JSX.Element {
 
   useEffect(() => {
 
-    // MyModule.show();
-
-
     Animated.timing(opacity, {
       toValue: 1,
       duration: 1000, // Adjust duration for animation speed
       useNativeDriver: true, // Use native driver for smoother performance on Android
     }).start();
   }, []);
-
-  const renderItem = ({ item, index }) => {
-    const interpolatedOpacity = opacity.interpolate({
-      inputRange: [0, index * 100], // Adjust based on list item height
-      outputRange: [0, 500],
-    });
-
-    return (
-      <View
-        style={{
-          // backgroundColor: 'green',
-          width: 50,
-          height: 50,
-        }}
-      >
-        <Animated.View style={{ opacity: interpolatedOpacity }}>
-          <Text>{item}</Text>
-        </Animated.View>
-      </View>
-    );
-  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -94,41 +60,7 @@ function App(): React.JSX.Element {
             }}
           ></View>
         } />
-      {/* <AnimatedHeader
-        LeftComponent={<View></View>}
-        RightComponent={<View></View>}
-      // Add any other props as needed
-      /> */}
 
-      {/* <AnimatedHeader
-        scrollY={scrollY}
-        leftComponentStyle={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        LeftComponent={
-          <View
-            style={{
-              width: "50%",
-              height: "50%",
-              backgroundColor: "pink",
-            }}
-          ></View>
-        }
-        rightComponentStyle={{
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-        RightComponent={
-          <View
-            style={{
-              width: "50%",
-              height: "50%",
-              backgroundColor: "pink",
-            }}
-          ></View>
-        }
-      /> */}
       <Animated.SectionList // Wrap SectionList with Animated.createAnimatedComponent
         sections={DATA}
         renderItem={({ item }) => <Text style={{ padding: 10 }}>{item}</Text>}
@@ -153,7 +85,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     backgroundColor: "#ecf0f1",
-    // padding: 8,
   },
   paragraph: {
     margin: 24,
